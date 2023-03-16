bf381816a522dceda7341eb8e42ba3a489a74d44
Pisarenko Dmitriy <pdp21u515@student.bmstu.ru>
2023-03-16 23:17:51 +0300

    output fix

--- lab_02/main.py
+++ lab_02/main.py
@@ -26,19 +26,4 @@ spline_natural = Spline(table, 0, 0)
 spline_left_edge_newton = Spline(table, newton_sec_dir_left, 0)
 spline_both_edge_newton = Spline(table, newton_sec_dir_left, newton_sec_dir_right)
 
-new_x = np.linspace(min(x) - 0.5, max(x) + 0.5, 150)
-
 print_table(table, x_single, spline_natural, spline_left_edge_newton, spline_both_edge_newton)
-
-fig, ax = plt.subplots()
-plt.title("Интерполяция с помощью кубических сплайнов")
-ax.plot(x, y, label='Изначальная таблица')
-ax.plot(new_x, [spline_natural.aproximate_value(i) for i in new_x], label='Сплайн (0, 0)')
-ax.plot(new_x, [spline_left_edge_newton.aproximate_value(i) for i in new_x], label='Сплайн (P``(x0), 0)')
-ax.plot(new_x, [spline_both_edge_newton.aproximate_value(i) for i in new_x], label='Сплайн (P``(x0), P``(xn))')
-ax.plot(new_x, [approximate_newton(table, i, 3) for i in new_x], label='Полином Ньютона 3-ей степени')
-
-ax.legend()
-ax.grid() 
-
-plt.show()
